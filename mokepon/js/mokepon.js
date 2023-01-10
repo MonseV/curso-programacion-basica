5c0b22e71a152119f0bb9f8fdd4adf0be956424f
Monse Vásquez <47286468+MonseV@users.noreply.github.com>
2023-01-10 14:49:14 -0600

    Desafío
    saber cual mascota seleccionamos con el getElementById().checked

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -1,9 +1,26 @@
-//funcion que ejecutara
-function seleccionarMascotaJugador(){
-  alert('SELECCIONASTE TU MASCOTA')
+function iniciarJuego() {
+
+  // se le va asigna el elemento por id,
+  let botonMascotaJugador = document.getElementById('boton-mascota')
+
+  // cuando se escucha el evento click se llama la funcion
+  botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
 }
 
-// se le va asigna el elemento por id,
-let botonMascotaJugador = document.getElementById('boton-mascota')
-// cuando se escucha el evento click se llama la funcion
-botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
+//funcion que ejecutara
+function seleccionarMascotaJugador() {
+  if((document.getElementById('hipodoge').checked) == true) {
+    alert('SELECCIONASTE A HIPODOGE COMO TU MASCOTA')
+  } else if ((document.getElementById('capipepo').checked) == true) {
+    alert('SELECCIONASTE A CAPIPEPO COMO TU MASCOTA')
+  } else if ((document.getElementById('ratigueya').checked) == true) {
+    alert('SELECCIONASTE A RATIGUEYA COMO TU MASCOTA')
+  }
+  else{
+    alert("NO SELECCIONASTE TU MASCOTA")
+  }
+}
+
+// cuando se escucha el evento cargar(carga todo el HTML) se llama la funcion
+window.addEventListener('load', iniciarJuego)
+
